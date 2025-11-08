76d5d8800ace725cad4c71f1d0966ed35c1dac0d
faridraka <faridrakapratama@gmail.com>
2025-11-08 10:19:29 +0700

    feat: Rate limit by arcjet

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 // Api Routes
 app.use('/api/v1/auth', authRouter);
