e5f5e8cab5a3778949d5120f746207199a891f2b
faridraka <faridrakapratama@gmail.com>
2026-08-17 21:37:21 +0700

    refactor: replace swagger route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import swaggerUi from 'swagger-ui-express'
 
-import { PORT } from "./config/env.js";
+import { SERVER_URL, PORT } from "./config/env.js";
 
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
@@ -22,20 +22,20 @@ app.use(cookieParser());
 app.use(arcjetMiddleware);
 
 // Api Routes
-app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
 app.use('/api/v1/workflows', workflowRouter);
 
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(errorMiddleware);
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the Subscription Tracker API!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Welcome to the Subscription Tracker API!");
+// });
 
 app.listen(PORT, async() => {
-  console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+  console.log(`Subscription Tracker API is running on ${SERVER_URL}`);
 
   await connectToDatabase();
 });
